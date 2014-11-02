6115d7d0260904befffed5e7895459b030cd2c9b
mirgags <markmiraglia@gmail.com>
2014-11-02 10:14:07 -0800

    added post twpm7

--- server.js
+++ server.js
@@ -36,10 +36,15 @@ function getTWPMKey() {
 function createTWPMTask (reqObject) {
     var passKey = getTWPMKey();
     console.log(passKey);
+/*
     var buff = new Buffer(passKey + ':X');
     var authStr = buff.toString('base64');
     console.log('encrypted: ' + authStr);
     console.log('unencrypted: ' + new Buffer(authStr, 'base64').toString());
+*/
+    var authStr = btoa(passKey + ':X');
+    console.log('encrypted: ' + authStr);
+    console.log('decrypted: ' + atob(authStr));
     var params = JSON.stringify({'todo-item': {
       	'content': reqObject.name,
         'description': reqObject.body,
