eb36098748a89e5cd8da9b2fd039f985967a175a
mirgags <markmiraglia@gmail.com>
2014-11-15 12:47:48 -0800

    pulled out twpm test

--- server.js
+++ server.js
@@ -33,113 +33,16 @@ function getTWPMKey() {
     return key.replace(/^\s+|\s+$/g, '');
 };
 
-function createTWPMTask (reqObject) {
-    var passKey = getTWPMKey();
-    console.log(passKey);
-    var buff = new Buffer(passKey + ':X');
-    var authStr = buff.toString('base64');
-    console.log('encrypted: ' + authStr);
-    console.log(JSON.stringify(reqObject));
-    var parameters = {'todo-item': {
-      	'content': reqObject['todo-item']['name'],
-        'description': reqObject['todo-item']['body'],
-        'responsible-party-id': reqObject['todo-item']['assigned_to_id'],
-        'start-date': reqObject['todo-item']['start_date'],
-        'due-date': reqObject['todo-item']['due_date'],
-        'estimated-minutes': reqObject['todo-item']['time'],
-        'creator-id': reqObject['todo-item']['creator_id'],
-        'responsible-party-ids': reqObject['todo-item']['other_assigned_ids']
-/*
-        'responsible-party-id': '86917',
-        'start-date': '20140909',
-        'due-date': '20140910',
-        'estimated-minutes': '99',
-        'creator-id': '84418',
-        'responsible-party-ids': '86917'
-*/
-	}};
-    console.log(JSON.stringify(parameters));
-    var options = {
-	host: 'clients.pint.com',
-//	host: 'requestb.in',
-	path: '/tasklists/354907/tasks.json',
-//	path: '/qooc93qo',
-	method: 'POST',
-	headers: {
-	    'Accept': 'application/json',
-	    'Content-Type': 'application/json',
-        'User-Agent': 'pint_integration',
-	    'Authorization': 'Basic ' + authStr
-        /*'Content-Length': parameters.length*/
-        }
-    };
-    var httpReq = http.request(options, function(response) {
-        console.log('STATUS: ' + response.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(response.headers));
-        response.setEncoding('utf8');
-        var body = '';
-        response.on('data', function (chunk) {
-            //console.log('BODY: ' + chunk);
-            body += chunk;
-        });
-    });
-    httpReq.on('error', function(e) {
-        console.log('request error: ' + e.message);
-    });
-    httpReq.write(JSON.stringify(parameters));
-    httpReq.end();
-};
-
-app.get('/testfile', function (req, res) {
-        var params = {'todo-item': {
-      	'name': 'New Test Task',
-        'body': 'This is the test description',
-        'assigned_to_id': '86917',
-        'start_date': '20140909',
-        'due_date': '20140910',
-        'time': '99',
-        'creator_id': '84418',
-        'other_assigned_ids': '86917'
-    }};
-    createTWPMTask(params);
-    res.end("<!DOCTYPE html><head></head><body>Req Sent</body></html>");
-});
-
-app.get('/test', function (req, res) {
-    console.log(getTWPMKey());
-    console.log('inbound header: ' + req.header['user-agent']);
+function createTWPMTask (reqObject, options) {
     var data = qs.stringify({
-	username: getTWPMKey(),
-	password: 'X'
+    	username: getTWPMKey(),
+	    password: 'X'
     });
     var buff = new Buffer(getTWPMKey() + ':X');
     var authStr = buff.toString('base64');
     console.log('encrypted: ' + authStr);
     console.log('unencrypted: ' + new Buffer(authStr, 'base64').toString());
-    var params = JSON.stringify({'todo-item': {
-      	'content': 'test task',
-        'description': 'test description',
-        'responsible-party-id': '86917',
-        'start-date': '20140901',
-        'due-date': '20140902',
-        'estimated-minutes': '99',
-        'creator-id': '82200',
-        'responsible-party-ids': '86917'
-	}});
-    var options = {
-	host: 'clients.pint.com',
-//	host: 'requestb.in',
-	path: '/tasklists/562384/tasks.json',
-//	path: '/qooc93qo',
-	method: 'POST',
-	headers: {
-	    'Accept': 'application/json',
-	    'Content-Type': 'application/json',
-        'User-Agent': 'pint_integration_middleware1.0',
-	    'Authorization': 'Basic ' + authStr,
-        'Content-Length': params.length
-        }
-    };
+    var params = JSON.stringify(reqObject);
     var httpReq = http.request(options, function (response) {
     	var str = '';
     	response.on('data', function(chunk) {
@@ -148,16 +51,48 @@ app.get('/test', function (req, res) {
     	    str += chunk;
     	});
     	response.on('end', function () {
-            var responseJSON = JSON.parse(str);
-            res.writeHead(200,{'Content-Type': 'text/html'}); 
-            res.end('<!DOCTYPE html><head></head><body>'+str+'</body>');
-    	});
+            return str;
+        });
         response.on('error', function(e) {
             console.log('ERROR: ' + e.message);
         });
     });
     httpReq.write(params);
     httpReq.end();
+};
+
+app.get('/test', function (req, res) {
+    console.log(getTWPMKey());
+    console.log('inbound header: ' + req.header['user-agent']);
+    var taskObject = {'todo-item': {
+      	'content': 'test task',
+        'description': 'test description',
+        'responsible-party-id': '86917',
+        'start-date': '20140901',
+        'due-date': '20140902',
+        'estimated-minutes': '99',
+        'creator-id': '82200',
+        'responsible-party-ids': '86917'
+        }
+    };
+    var taskOptions = {
+    	host: 'clients.pint.com',
+    //	host: 'requestb.in',
+    	path: '/tasklists/562384/tasks.json',
+    //	path: '/qooc93qo',
+    	method: 'POST',
+    	headers: {
+    	    'Accept': 'application/json',
+    	    'Content-Type': 'application/json',
+            'User-Agent': 'pint_integration_middleware1.0',
+    	    'Authorization': 'Basic ' + authStr,
+            'Content-Length': params.length
+            }
+        };
+	};
+    var responseStr = createTWPMTask(taskObject, taskOptions);
+    res.writeHead(200,{'Content-Type': 'text/html'}); 
+    res.end('<!DOCTYPE html><head></head><body>'+responseStr+'</body>');
 //    console.log('response: ' + responseJSON);
 });
 
