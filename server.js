be9a8fb9655109f073a49c1d03503abe1a5c5e5c
mirgags <markmiraglia@gmail.com>
2015-01-29 17:46:07 -0800

    new effort4

--- server.js
+++ server.js
@@ -248,7 +248,7 @@ function createTWPMTask (taskListID, theRequest, theResponse, callback) {
                     }
                 };
     console.log(JSON.stringify(options));
-    console.log('theRequest: ' + JSON.stringify(theRequest);
+    console.log('theRequest: ' + JSON.stringify(theRequest));
     var twpmKey = getKey('twpm');
     var buff = new Buffer(twpmKey + ':X');
     var authStr = buff.toString('base64');
